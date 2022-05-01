f28f4644bc994d427ae3b63d2dfe28cab0c37e2f
Sumendra Chowdary <sumendrachowdary@gmail.com>
2022-05-01 09:44:49 +0530

    First Copy

---
+++ schoolmanagement-config/src/main/java/com/sumendra/schoolmanagementconfig/SchoolmanagementConfigApplication.java
@@ -0,0 +1,13 @@
+package com.sumendra.schoolmanagementconfig;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SchoolmanagementConfigApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SchoolmanagementConfigApplication.class, args);
+	}
+
+}
---
+++ schoolmanagement-config/src/test/java/com/sumendra/schoolmanagementconfig/SchoolmanagementConfigApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sumendra.schoolmanagementconfig;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SchoolmanagementConfigApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
