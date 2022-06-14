7bd3dc3cecb113e85f316956c2ba47298e8f135e
Sumendra Chowdary <sumendrachowdary@gmail.com>
2022-06-14 19:54:32 +0530

    Swagger Configuration for School controller

--- src/main/java/com/sumendra/schoolmanagement/controller/SchoolController.java
+++ src/main/java/com/sumendra/schoolmanagement/controller/SchoolController.java
@@ -1,6 +1,9 @@
 package com.sumendra.schoolmanagement.controller;
 
 import com.sumendra.schoolmanagement.dto.School;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
@@ -9,7 +12,15 @@ import javax.validation.Valid;
 @RequestMapping("/school/api/")
 public class SchoolController {
 
+    @ApiOperation(nickname = "getSchoolDetails",value="Getting School Details", notes="Retrieves all the details of the school depending up on the ID of the school passed as path variable")
+    @ApiResponses(
+            value={
+                    @ApiResponse(code=200,message="Success, Ended OK", response = String.class),
+                    @ApiResponse(code=400, message= "Bad Request", response = String.class)
+            }
+    )
     @GetMapping(value = "/getSchoolDetails", produces = "application/json")
+    //@LogExecutionTime()
     public School getSchoolDetails ()
     {
         return new School(1,"STVN","Chinnayagudem");
